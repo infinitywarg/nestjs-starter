03afe4b3b5e2a7bc46c9952a1f08a9362c1a3ad5
infinitywarg <ganesh.anantwar@outlook.com>
2023-04-20 15:43:19 +0530

    add health route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Version('1')
+  @Get('health')
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHealth();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
+  app.enableCors();
+  app.use(helmet());
+  app.setGlobalPrefix('/api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(3000);
 }
 bootstrap();
